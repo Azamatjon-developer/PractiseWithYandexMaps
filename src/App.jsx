0d94d75849e10d6f0406cc2d2f4694aaa95b2662
Azamatjon <azamatergasev01@gmail.com>
2025-04-07 10:55:35 +0500

    projectstarts

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MapComponents from './components/MapComponents'
+
+function App() {
+
+  return (
+    <>
+    <MapComponents/>
+    </>
+  )
+}
+
+export default App
